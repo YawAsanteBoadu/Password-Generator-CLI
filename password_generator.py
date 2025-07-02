9b30b20505a5eeaae0166068b9bd6a8cf10c53a9
Yaw Asante Boadu <yawasante95@yahoo.com>
2025-07-02 05:45:49 +0000

    feat: Implemented user interaction and customizable password length

--- password_generator.py
+++ password_generator.py
@@ -16,9 +16,31 @@ def generate_password(length=12, use_uppercase=True, use_lowercase=True, use_num
     if use_specials:
         char_pools.append(string.punctuation)
     if not char_pools:
-        raise ValueError ("At leasr one character type must be selected")
+        raise ValueError ("At least one character type must be selected")
     
     password = [random.choice(pool) for pool in char_pools]
 
     all_chars = ''.join(char_pools)
     password.extend(random.choice(all_chars, k=length - len(password)))
+
+    random.shuffle(password)
+
+    return "" .join(password)
+
+def main():
+    print("Welcoome to the password generator")
+    try:
+        length = int(input("Enter desired password lenght (8-20):  ")) 
+        use_uppercase = input("include uppercase letters? Yes/No:  ") == "y"
+        use_lowercase = input("Inlude lowercase letters? Yes/No:  ") == "y"
+        use_numbers = input("Include Numbers? Yes/ No:  ") == "y"
+        use_specials = input("Include special characters? Yes/No:  ") == "y"
+    
+        password = generate_password(length, use_uppercase, use_lowercase, use_numbers, use_specials)
+
+        print(f'Generated Password: {password}')
+        print("Password copied to clipboard")
+
+
+
+
